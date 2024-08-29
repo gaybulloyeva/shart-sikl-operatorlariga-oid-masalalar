54ba84a296323e38afba6f28fd9ad4bc8041744f
new <gaybullayevamrayhona718@gmail.com>
2024-08-29 13:35:13 +0500

    while13

---
+++ while13/script.js
@@ -0,0 +1,14 @@
+function solve() {
+    let a = +prompt()
+    let k = 1
+    let sum = 0
+
+    while (sum < a) {
+        sum = sum + (1 / k)
+        k++
+    }
+
+    console.log("1 dan " + (k - 1) + " gacha bo'lgan yig'indi: " + sum)
+}
+
+solve()
